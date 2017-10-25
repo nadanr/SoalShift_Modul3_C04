6d4e7b45b91637bd88883142f61281933dfd3db4
nadanr <nadarosadi22@gmail.com>
2017-10-25 16:39:32 +0700

    fix alhamdulillah

--- soal5.c
+++ soal5.c
@@ -4,11 +4,6 @@
 #include <pthread.h>
 #include <string.h>
  
-//pthread_t tid1;
-//pthread_t tid2;
-//int status;
-
-//char simpan2[2000];
 FILE *in;
 
 void* hitung( void *arg ){
@@ -26,18 +21,7 @@ void* hitung( void *arg ){
         while(simpan[count]!='\0'){
         count++;
     }
-    //printf("count = %d\n\n", count);
-
-    /*for(j=0; j<=count-4; j++){
-        i = (simpan[j] == 'i' || simpan[j] == 'I' );
-        f = (simpan[j+1] == 'f' || simpan[j+1] == 'F' );
-        a = (simpan[j+2] == 'a' || simpan[j+2] == 'A' );
-        h = (simpan[j+3] == 'h' || simpan[j+3] == 'H' );
-        //space = (simpan[j+4] == ' ' || simpan[j+4] == '\0');
-        if((i && f && a && h ) ==1){
-            countIf++;
-        }
-    }*/
+   
      wordLen = strlen(cari);//find length of the word
 
         for(i=0; i<count-wordLen; i++){
@@ -59,57 +43,6 @@ void* hitung( void *arg ){
     printf("%s = %d\n", cari, countIf);
     //return;
 }
-    
-    /*while(fgets(simpan, 1000, in)!=NULL){
-        if(strstr(simpan,"Ifah")!=NULL){
-            //printf("A match found on line number = %d\n", line_num);
-            //printf("\n%s\n", simpan);
-            countIf++;        
-        }
-        line_num ++;
-    }*/
-    //status = 1;
-    
-    //printf("Ifah = %d\n", countIf);
-    
-
-
-/*void* hitungFina( void *arg ){
-    int countFi=0;
-    int count=0;
-    int j, f, i, n, a, space;
-    while(status!=1){
-    
-    }
-    
-    inn = fopen("/home/nadanr/SISOP/Novel.txt", "r");
-    
-    while(fgets(simpan2, 2000, inn)!=NULL){
-        while(simpan2[count]!='\0'){
-        count++;
-    }
-    //printf("count = %d\n\n", count);
-
-    for(j=0; j<=count-4; j++){
-        f = (simpan2[j] == 'f' || simpan2[j] == 'F' );
-        i = (simpan2[j+1] == 'i' || simpan2[j+1] == 'I' );
-        n = (simpan2[j+2] == 'n' || simpan2[j+2] == 'N' );
-        a = (simpan2[j+3] == 'a' || simpan2[j+3] == 'A' );
-        //space = (simpan2[j+4] == ' ' || simpan2[j+4] == '\0');
-        if((f && i && n && a )==1){
-            countFi++;
-        }
-    }
-}
-    /*while(fgets(simpan2, 1000, inn)!=NULL){
-        if(strstr(simpan2, "Fina")){
-            countFi++;        
-        }    
-    }*/
-    /*fclose(inn);
-    printf("Fina = %d\n", countFi);
-    
-}*/
 
 int main(int j, char **arg)
 {
